81849871987316b5712e2501769b42c72fe6d1df
obunga321 <clavakoka07@gmail.com>
2025-02-10 00:41:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Добавляем интерактивность для кнопки
+const buyButton = document.querySelector('.buy-button');
 
+buyButton.addEventListener('mouseover', () => {
+  buyButton.textContent = "Встать на волну хайпа";
+});
+
+buyButton.addEventListener('mouseout', () => {
+  buyButton.textContent = "Купить токен";
+});
