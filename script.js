f46182d0106e1e01002ba6fe265c9ee755d73e3a
obunga321 <clavakoka07@gmail.com>
2025-02-10 00:38:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 
-const buyButton = document.querySelector('.buy-button');
-
-buyButton.addEventListener('mouseover', () => {
-  buyButton.textContent = "Встать на волну хайпа x2";
-});
-
-buyButton.addEventListener('mouseout', () => {
-  buyButton.textContent = "Купить токен";
-});
