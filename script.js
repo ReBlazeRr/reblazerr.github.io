0bc68c86573fe2f09b016098300bc1cb80a8d988
obunga321 <clavakoka07@gmail.com>
2025-02-10 01:25:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// Добавляем интерактивность для кнопки
-const buyButton = document.querySelector('.buy-button');
+// Пример изменения статуса плана
+const cards = document.querySelectorAll('.card');
 
-buyButton.addEventListener('mouseover', () => {
-  buyButton.textContent = "Встать на волну хайпа";
-});
-
-buyButton.addEventListener('mouseout', () => {
-  buyButton.textContent = "Купить токен";
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    if (card.classList.contains('in-progress')) {
+      card.classList.remove('in-progress');
+      card.classList.add('done');
+      card.querySelector('.status').textContent = 'DONE';
+    } else if (card.classList.contains('done')) {
+      card.classList.remove('done');
+      card.classList.add('failed');
+      card.querySelector('.status').textContent = 'FAILED';
+    } else {
+      card.classList.remove('failed');
+      card.classList.add('in-progress');
+      card.querySelector('.status').textContent = 'IN PROGRESS';
+    }
+  });
 });
