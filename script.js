b85e787236d03c2ce05fb3ca0e0f769d14b65cfa
obunga321 <clavakoka07@gmail.com>
2025-02-10 16:47:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ window.onresize = hideHeaderOnMobile;
 // Загружаем звук
 const hoverOnBuyButtonSound = new Audio("./sounds/yanenormis.mp3"); // Укажи путь к файлу
 
-hoverOnBuyButtonSound.volume = 4;
+hoverOnBuyButtonSound.volume = 1;
 
 const buyButton = document.querySelector(".buy-button");
 
